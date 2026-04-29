c66255c5b0587bfd3fb30a4363033ba651f62912
Viraj Dhorajiya <pviraj98@gmail.com>
2026-04-29 09:43:27 +0530

    fix(stats): 19+ states suffix

--- src/data/stats.js
+++ src/data/stats.js
@@ -3,6 +3,6 @@ export const stats = [
   { value: '15,000', suffix: '+', label: 'Clients served' },
   { value: '1,000', suffix: '+', label: 'MNC engagements' },
   { value: '50', suffix: '+', label: 'Person team' },
-  { value: '19', suffix: '', label: 'States served' },
+  { value: '19', suffix: '+', label: 'States served' },
   { value: '6', suffix: '+', label: 'Countries' },
 ]
